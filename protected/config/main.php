ebece51fee18b4024d58990fdff15cd08818e0ee
Alex Crooks <alex@alexcrooks.com>
2013-10-06 14:30:21 -0700

    added YiiBooster component

--- protected/config/main.php
+++ protected/config/main.php
@@ -10,7 +10,10 @@ return array(
 	'name'=>'COPUS',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array(
+        'log',
+        'bootstrap',
+    ),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -67,6 +70,9 @@ return array(
 				*/
 			),
 		),
+        'bootstrap' => array(
+            'class' => 'application.extensions.bootstrap.components.Bootstrap',
+        ),
 	),
 
 	// application-level parameters that can be accessed
